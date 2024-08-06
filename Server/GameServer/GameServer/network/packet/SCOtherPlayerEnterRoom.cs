2b4c58429a6a670f7f0f31dbb98b8f48ed5b2083
sunzhe <932688274@qq.com>
2024-08-06 17:19:22 +0800

    重构!大胆删除！

--- Server/GameServer/GameServer/network/packet/SCOtherPlayerEnterRoom.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-public class SCOtherPlayerEnterRoom : SCPacketBase
-{
-    public override int Id => PacketID.SCOtherPlayerEnterRoom;
-
-}
-
